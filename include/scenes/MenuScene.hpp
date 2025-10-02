1a5d82798c4f1d7f5cdcb7c6fa355797aef1d74b
Vadym T <vadim003600@gmail.com>
2025-10-02 18:33:47 -0500

    Add MenuScene header with UI elements
    - Define MenuScene class inheriting from Scene
    - Declare constructor with scene name, size, and parent widget
    - Add private members: startButton, exitButton, and gameTitle QLabel
    - Prepare for future paintEvent override (commented out)

---
+++ include/scenes/MenuScene.hpp
@@ -0,0 +1,20 @@
+#pragma once 
+
+#include <QPushButton>
+#include <QLabel>
+#include <string>
+
+#include "scenes/Scene.hpp"
+
+class MenuScene : public Scene
+{
+public:
+    MenuScene(std::string sceneName, int width, int height, QWidget *parent = nullptr);
+
+    //void paintEvent(QPaintEvent *event) override;
+
+private:
+    QPushButton *startButton;
+    QPushButton *exitButton;
+    QLabel      *gameTitle;
+};
