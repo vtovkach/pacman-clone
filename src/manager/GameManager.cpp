5c38bd5c09abe2e52995cf849049ba2431dd6c30
Vadym T <vadim003600@gmail.com>
2025-10-03 00:28:59 -0500

    Add MenuScene setup and exit handling in GameManager
    - Initialize currentScene with MenuScene in constructor
    - Connect MenuScene's exitGame signal to GameManager's closeGame slot
    - Implement closeGame() to close the current scene window
    - Add placeholder for changeScene() logic for future scene transitions

--- src/manager/GameManager.cpp
+++ src/manager/GameManager.cpp
@@ -1,11 +1,18 @@
 #include "include/manager/GameManager.hpp"
+#include <QObject>
+#include <QApplication>
+#include <iostream>
 
 constexpr int FRAME_WIDTH = 784; 
 constexpr int FRAME_HEIGHT = 868;
 
 GameManager::GameManager()
 {
+    // Create MenuScene 
     currentScene = std::make_unique<MenuScene>("Pac-Man Menu", FRAME_WIDTH, FRAME_HEIGHT, nullptr);
+
+    // Connect current scene to signals  
+    QObject::connect(currentScene.get(), &MenuScene::exitGame, [this]() { closeGame(); });
 }
 
 void GameManager::runPacMan()
@@ -13,7 +20,13 @@ void GameManager::runPacMan()
     currentScene->show();
 }
 
+void GameManager::closeGame()
+{
+    currentScene->close();
+}
+
 void GameManager::changeScene()
 {
-    // ... 
+    // Change Scene Logic 
+    // 
 }
