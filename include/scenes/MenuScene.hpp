36c4e3c0700504504b4210196e313a01981cf686
Vadym T <vadim003600@gmail.com>
2025-10-03 02:04:42 -0500

    Enables paint event for MenuScene
    Add the paintEvent function in the MenuScene class, enabling custom drawing and rendering within the menu.

--- include/scenes/MenuScene.hpp
+++ include/scenes/MenuScene.hpp
@@ -11,7 +11,7 @@ class MenuScene : public Scene
 public:
     MenuScene(std::string sceneName, int width, int height, QWidget *parent = nullptr);
 
-    //void paintEvent(QPaintEvent *event) override;
+    void paintEvent(QPaintEvent *event) override;
 
 private:
     QPushButton *startButton;
