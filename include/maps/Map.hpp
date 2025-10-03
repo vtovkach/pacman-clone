5eabe760de2c96cce4e43c752060f737ad712651
Vadym T <vadim003600@gmail.com>
2025-10-03 17:41:08 -0500

    Outlined Map Class
    Map class is responsible for maintaining the static part the map.

---
+++ include/maps/Map.hpp
@@ -0,0 +1,54 @@
+#pragma once 
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <list>
+#include <utility>
+#include <memory>
+
+#include "include/entities/Entity.hpp"
+#include "include/entities/PacMan.hpp"
+#include "include/entities/Pellet.hpp"
+#include "include/entities/ghosts/Ghost.hpp"
+#include "include/entities/ghosts/Blinky.hpp"
+#include "include/entities/ghosts/Clyde.hpp"
+#include "include/entities/ghosts/Inky.hpp"
+#include "include/entities/ghosts/Pinky.hpp"
+
+#include "include/entities/Entity.hpp"
+
+// Origin point of the pacman map  
+constexpr std::pair<int, int> ORIGIN_POINT = {20, 20};
+// Tile size in pixels 
+constexpr int TILE_SIZE = 27; 
+
+// Map grid dimensions 
+constexpr int GRID_W = 28;
+constexpr int GRID_H = 30;
+
+class Map
+{
+public:
+    Map(std::string& mapFilePath, PacMan& pacman, std::vector<Pellet>& pellets, std::vector<Ghost>& ghosts);
+    // Public interface  
+    // ... 
+
+private:
+
+    struct Node
+    {
+        bool isWall; 
+        bool isSpace; 
+        std::pair<int, int> gridCordinate;
+        std::pair<int,int> pixelCordinate; 
+        std::vector<Node*> neigbors;
+    };
+
+    std::vector<std::unique_ptr<Node>> gameMapGraph;
+    
+    PacMan& pacman;
+    std::vector<Pellet>& pellets; 
+    std::vector<Ghost>& ghosts;
+};
