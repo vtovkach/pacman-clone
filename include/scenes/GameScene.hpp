97da73ecc078e8db78875e6180fc0b05bdb62d5d
Vadym T <vadim003600@gmail.com>
2025-10-05 02:22:59 -0500

    Add score labels and references to GameScene
    - Added QLabel pointers for current and highest score display
    - Introduced vector of QLabel pointers for life indicators
    - Changed constructor to accept references to current and high scores
    - Stored references to scores as member variables for real-time updates

--- include/scenes/GameScene.hpp
+++ include/scenes/GameScene.hpp
@@ -2,16 +2,24 @@
 
 #include "include/scenes/Scene.hpp"
 #include "include/maps/Map.hpp"
+#include <QLabel>
 
 class GameScene : public Scene
 {
 public:
-    GameScene(const std::string& sceneName, int width, int height, QWidget* parent = nullptr);
+    GameScene(const std::string& sceneName, int width, int height, QWidget* parent, int& curScore, int& highScore);
     
     void paintEvent(QPaintEvent *event) override;
 
 private:
-    // Game objects 
-
     Map gameMap;
+
+    QLabel *highestScoreLabel;
+    QLabel *highestScoreNum;
+    QLabel *currentScoreLabel;
+    QLabel *currentScoreNum;
+    std::vector<QLabel*> lifes; 
+    
+    int& curScore;
+    int& highScore;  
 };
