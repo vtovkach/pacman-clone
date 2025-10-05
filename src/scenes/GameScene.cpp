e0e4790e67669f4b2b04c669f1b542484bdfb1f9
Vadym T <vadim003600@gmail.com>
2025-10-05 02:22:59 -0500

    Add score and lives UI elements to GameScene
    - Pass current and high score references to GameScene constructor
    - Display 3 life icons at fixed positions
    - Add styled QLabel widgets for "High Score" and current "Score" with initial values
    - Position score labels clearly on the scene for better game status visibility

--- src/scenes/GameScene.cpp
+++ src/scenes/GameScene.cpp
@@ -4,11 +4,39 @@
 #include <QPixmap>
 #include <QPen>
 #include <QColor>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
 
-GameScene::GameScene(const std::string& sceneName, int width, int height, QWidget* parent) : Scene(sceneName, width, height, parent), gameMap("assets/maps/level1.map")
+GameScene::GameScene(const std::string& sceneName, int width, int height, QWidget* parent, int& curScore, int& highScore) : Scene(sceneName, width, height, parent), gameMap("assets/maps/level1.map"), curScore(curScore), highScore(highScore)
 {
-    // TODO 
-    // ... 
+    // Set up lifes labels 
+    int initX = 20;
+    int initY = 35;
+    for(int i = 0; i < 3; i++)
+    {
+        QLabel *life = new QLabel(this);
+        life->setPixmap(QPixmap("assets/pacman-left/1.png"));
+        life->setScaledContents(true);
+        life->setFixedSize(32, 32);
+        life->move(initX, initY);
+        initX += 40;
+    }
+
+    // Set up highest score  
+    highestScoreLabel = new QLabel("High Score:", this);
+    highestScoreNum = new QLabel("0", this);
+    highestScoreLabel->setStyleSheet("color: white; font-size: 25px;");
+    highestScoreNum->setStyleSheet("color: white; font-size: 25px;");
+    highestScoreLabel->move(285, 40);
+    highestScoreNum->move(430, 40);
+
+    // Setup current score 
+    currentScoreLabel = new QLabel("Score:", this);
+    currentScoreNum = new QLabel("0", this);
+    currentScoreLabel->setStyleSheet("color: white; font-size: 25px;");
+    currentScoreNum->setStyleSheet("color: white; font-size: 25px;");
+    currentScoreLabel->move(600, 40);
+    currentScoreNum->move(680, 40);
 }
 
 void GameScene::paintEvent(QPaintEvent *event)
