41921e7ddf42cd4719d7cc8ff26c7bc290bba2c5
Vadym T <vadim003600@gmail.com>
2025-10-06 00:11:10 -0500

    Crated Entity Parent class

--- include/entities/Entity.hpp
+++ include/entities/Entity.hpp
@@ -1,6 +1,26 @@
 #pragma once 
 
-class Entity
-{
+#include <utility>
+#include <QPixmap>
 
+class Entity 
+{
+public:
+    Entity(int speed, std::pair<int, int> gridCord, std::pair<int, int> pixelCord);
+
+protected:
+
+    enum class Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    };
+
+    int speed;
+    Direction direction; 
+    std::pair<int, int> gridCord; 
+    std::pair<int, int> pixelCord; 
+    QPixmap pixmap;
 };
