40c57066282ce003535057e069cd46794a9e97f8
Vadym T <vadim003600@gmail.com>
2025-10-06 00:11:27 -0500

    Created PacMan Class child of Entity

--- include/entities/PacMan.hpp
+++ include/entities/PacMan.hpp
@@ -1,6 +1,15 @@
 #pragma once 
 
-class PacMan
-{
+#include "include/entities/Entity.hpp"
 
+class PacMan : public Entity
+{
+public:
+    PacMan();
+    
+private: 
+    std::vector<QPixmap> pacmanDown;
+    std::vector<QPixmap> pacmanUp;
+    std::vector<QPixmap> pacmanLeft;
+    std::vector<QPixmap> pacmanRight;
 };
