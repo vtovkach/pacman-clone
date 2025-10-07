da2a4fff7b5e61d9505ea9ae19ba0673e6a661de
Vadym T <vadim003600@gmail.com>
2025-10-07 00:30:57 -0500

    Load directional PacMan sprites on init
    - Load QPixmap assets for all four directions (Down, Up, Left, Right) into respective vectors
    - Set initial activePixmap to the first left-facing sprite for drawing
    - Enables directional animation by preloading all PacMan assets on construction

--- src/entities/PacMan.cpp
+++ src/entities/PacMan.cpp
@@ -1,6 +1,38 @@
 #include "include/entities/PacMan.hpp"
 
+
+// Use parent's pixmap as the active one that will be used in drawing on the screen 
+
 PacMan::PacMan(int speed, std::pair<int, int> gridCord, std::pair<int, int> pixelCord) : Entity(speed, gridCord, pixelCord)
 {
-    // ...
+    // Load Down 
+    for(auto& asset : PacmanAssets::DOWN)
+    {
+        QPixmap pacmanPixMap(asset);
+        this->pacmanDown.push_back(pacmanPixMap);
+    }
+
+    // Load Up
+    for(auto& asset : PacmanAssets::UP)
+    {
+        QPixmap pacmanPixMap(asset);
+        this->pacmanUp.push_back(pacmanPixMap);
+    }
+
+    // Load Left 
+    for(auto& asset : PacmanAssets::LEFT)
+    {
+        QPixmap pacmanPixMap(asset);
+        this->pacmanLeft.push_back(pacmanPixMap);
+    }
+
+    // Load Right
+    for(auto& asset : PacmanAssets::RIGHT)
+    {
+        QPixmap pacmanPixMap(asset);
+        this->pacmanRight.push_back(pacmanPixMap);
+    }
+
+    // Load left pacman into active pixmap 
+    activePixmap = pacmanLeft[0];
 }
