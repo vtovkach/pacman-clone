3f26640225d7255870cb40d0d74510b75d29242f
Vadym T <vadim003600@gmail.com>
2025-10-07 00:30:57 -0500

    Add PacMan sprite asset paths
    - Define inline constexpr arrays for PacMan animation frames
    - Organize asset paths by direction: DOWN, UP, LEFT, RIGHT
    - Place arrays inside PacmanAssets namespace for clarity and scope control

--- include/entities/PacMan.hpp
+++ include/entities/PacMan.hpp
@@ -2,6 +2,14 @@
 
 #include "include/entities/Entity.hpp"
 
+namespace PacmanAssets 
+{
+    inline constexpr const char* DOWN[]  = {"assets/pacman-down/1.png",  "assets/pacman-down/2.png",  "assets/pacman-down/3.png"};
+    inline constexpr const char* UP[]    = {"assets/pacman-up/1.png",    "assets/pacman-up/2.png",    "assets/pacman-up/3.png"};
+    inline constexpr const char* LEFT[]  = {"assets/pacman-left/1.png",  "assets/pacman-left/2.png",  "assets/pacman-left/3.png"};
+    inline constexpr const char* RIGHT[] = {"assets/pacman-right/1.png", "assets/pacman-right/2.png", "assets/pacman-right/3.png"};
+}
+
 class PacMan : public Entity
 {
 public:
