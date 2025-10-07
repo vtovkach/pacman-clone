c32bb73ad6241f685fe1a954e78c7022db4f9bf6
Vadym T <vadim003600@gmail.com>
2025-10-07 00:30:57 -0500

    Refactor Entity class: move Direction enum out
    - Moved Direction enum outside Entity class for broader accessibility
    - Added getter methods for direction, gridCord, and pixelCord
    - Renamed pixmap member to activePixmap for clarity and consistency

--- include/entities/Entity.hpp
+++ include/entities/Entity.hpp
@@ -3,24 +3,35 @@
 #include <utility>
 #include <QPixmap>
 
+enum class Direction
+{
+    Up,
+    Down,
+    Left,
+    Right
+};
+
 class Entity 
 {
 public:
     Entity(int speed, std::pair<int, int> gridCord, std::pair<int, int> pixelCord);
 
+    Direction getDirection(){
+        return this->direction;
+    }
+
+    std::pair<int, int> getGridCord(){
+        return this->gridCord;
+    }
+
+    std::pair<int, int> getPixelCord(){
+        return this->pixelCord;
+    }
+    
 protected:
-
-    enum class Direction
-    {
-        Up,
-        Down,
-        Left,
-        Right
-    };
-
     int speed;
     Direction direction; 
     std::pair<int, int> gridCord; 
     std::pair<int, int> pixelCord; 
-    QPixmap pixmap;
+    QPixmap activePixmap;
 };
